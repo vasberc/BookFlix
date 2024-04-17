6be3aa2ba626ada71e549c70e229600d868307e2
Vasilis Berkovits Ioannidis <vasileios.b@weblicity.com>
2024-04-17 19:36:24 +0300

    data local ok

--- data_local/build.gradle.kts
+++ data_local/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.library")
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -24,18 +25,24 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+
+        isCoreLibraryDesugaringEnabled = true
     }
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
 }
 
-dependencies {
+ksp {
+    arg("KOIN_CONFIG_CHECK", "true")
+}
 
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+dependencies {
+    ksp(libs.koinKsp)
+    ksp(libs.roomKsp)
+    implementation(libs.bundles.core)
+    implementation(libs.bundles.dataLocal)
+    coreLibraryDesugaring(libs.desugaring)
+    testImplementation(libs.bundles.testing)
+    androidTestImplementation(libs.bundles.androidTesting)
 }
