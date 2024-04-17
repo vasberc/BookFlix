b403b84ea5527a6b1eb27a24986d9096385d27f5
Vasilis Berkovits Ioannidis <vasileios.b@weblicity.com>
2024-04-17 20:30:51 +0300

    presentation module ok

--- app/src/main/java/com/vasberc/app/BookFlixApp.kt
+++ app/src/main/java/com/vasberc/app/BookFlixApp.kt
@@ -1,6 +1,23 @@
 package com.vasberc.app
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
 
 class BookFlixApp: Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin()
+    }
+
+    private fun startKoin() {
+        org.koin.core.context.startKoin {
+            androidContext(this@BookFlixApp)
+            modules(
+                PresentationModule().module,
+                DomainModule().module,
+                DataRemoteModule().module,
+                DataLocalModule().module
+            )
+        }
+    }
 }
---
+++ presentation/src/main/java/com/vasberc/bookflix/di/PresentationModule.kt
@@ -0,0 +1,8 @@
+package com.vasberc.bookflix.di
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("com.vasberc.presentation")
+class PresentationModule
