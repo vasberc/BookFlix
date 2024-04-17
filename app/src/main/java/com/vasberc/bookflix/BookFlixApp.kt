cf182f170399b1ec16d3047bce106733c60a93cf
Vasilis Berkovits Ioannidis <vasileios.b@weblicity.com>
2024-04-17 20:42:50 +0300

    ok

--- app/src/main/java/com/vasberc/bookflix/BookFlixApp.kt
+++ app/src/main/java/com/vasberc/bookflix/BookFlixApp.kt
@@ -1,7 +1,13 @@
 package com.vasberc.bookflix
 
 import android.app.Application
+import com.vasberc.data_local.di.DataLocalModule
+import com.vasberc.data_remote.di.DataRemoteModule
+import com.vasberc.domain.di.DomainModule
+import com.vasberc.presentation.di.PresentationModule
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import org.koin.ksp.generated.module
 
 class BookFlixApp: Application() {
     override fun onCreate() {
@@ -10,7 +16,7 @@ class BookFlixApp: Application() {
     }
 
     private fun startKoin() {
-        org.koin.core.context.startKoin {
+        startKoin {
             androidContext(this@BookFlixApp)
             modules(
                 PresentationModule().module,
